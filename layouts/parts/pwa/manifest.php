01df6bd4b3f51f1fd4b2f702373932f828bd0892
Ben Rainir <benrainir@gmail.com>
2021-03-01 21:00:00 -0300

    corrigindo link amnifest

--- layouts/parts/pwa/manifest.php
+++ layouts/parts/pwa/manifest.php
@@ -1,3 +1,3 @@
-<link rel="shortcut icon" href="<?php $this->asset('pwa/files/manifest.json') ?>" />
+<link rel="manifest" href="<?php $this->asset('pwa/files/manifest.json') ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="shortcut icon" type="image/x-icon" href="<?php $this->asset('pwa/img/app-512x512.png') ?>" />
