b2b8eac8e082bff75f79426fac26887fd7e81aaf
Ben Rainir <benrainir@gmail.com>
2021-03-01 21:48:38 -0300

    corrigindo imagens

--- layouts/parts/pwa/manifest.php
+++ layouts/parts/pwa/manifest.php
@@ -6,4 +6,4 @@
 <meta name="mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-title" content="Add to Home">
 <link rel="shortcut icon" sizes="16x16" href="<?php $this->asset('pwa/img/icon-16x16.png') ?>">
-<link rel="shortcut icon" sizes="192x192" href="<?php $this->asset('pwa/img/icon-192x192.png') ?>">
+<link rel="shortcut icon" sizes="192x192" href="<?php $this->asset('pwa/img/icon-192x192.png')?>">
