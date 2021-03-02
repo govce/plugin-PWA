ad603b474236572a6b7a8fa0e3605f9cea50fed0
Ben Rainir <benrainir@gmail.com>
2021-03-01 22:38:55 -0300

    tentativa 5 serviceworker dentro de assets

--- assets/js/a2h.js
+++ assets/js/a2h.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     /* Register service worker to control making site work offline */
     if('serviceWorker' in navigator) {
         navigator.serviceWorker
-            .register('./serviceWorker.js')
+            .register('./assets/pwa/files/serviceWorker.js')
             .then(function() { console.log('Service Worker Registered'); });
     }
 });
